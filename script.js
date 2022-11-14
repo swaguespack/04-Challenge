a4e6ce183e3092467ef3d8c5d7a0027f71629242
shellby.miller <shellby.miller@gmail.com>
2022-11-13 20:56:25 -0500

    added object of questions to javascript

---
+++ script.js
@@ -0,0 +1,37 @@
+var quizQuestions = [{
+    question: "Commonly used data types DO NOT include:",
+    choiceA: "strings",
+    choiceB: "booleans",
+    choiceC: "alerts",
+    choiceD: "numbers",
+    correctAnswer: "c" },
+{
+    questions: "The condition of an if / else statement is enclosed within ___.",
+    choiceA: "quotes",
+    choiceB: "curly brackets",
+    choiceC: "parentheses",
+    choiceD: "square brackets",
+    correctAnswer: "c" },
+    
+{
+    questions: "Arrays in Javascript can be used to store ___.",
+    choiceA: "numbers and strings",
+    choiceB: "other arrays",
+    choiceC: "booleans",
+    choiceD: "all of the above",
+    correctAnswer: "d" },
+{
+    questions: "String values must be enclosed within ___ when being assigned to variables.",
+    choiceA: "commas",
+    choiceB: "curly brackets",
+    choiceC: "quotes",
+    choiceD: "parentheses",
+    correctAnswer: "d" },
+{
+    questions: "A very useful tool used during development and debugging for printing content to the debugger is___.",
+    choiceA: "Javascript",
+    choiceB: "terminal/bash",
+    choiceC: "for loops",
+    choiceD: "console.log",
+    correctAnswer: "d" }
+]
